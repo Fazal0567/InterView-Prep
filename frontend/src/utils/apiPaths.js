c7cda80160afacdf711f7a8aaa6b5316936e405a
Mohd Fazal Ali <fazalalicontribute@gmail.com>
2025-07-06 23:32:19 +0000

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 // BASE URL
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://interview-prep-rgs8.onrender.com';
 
 // API ENDPOINT PATHS
 export const API_PATHS = {
