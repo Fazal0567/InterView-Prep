5bdead0758ed05e63da61a54f0eb38dfd60d2c70
Mohd Fazal Ali <fazalalicontribute@gmail.com>
2025-07-09 00:59:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,42 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
-
 const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
-
-const {
-  generateInterviewQuestions,
-  generateConceptExplanation,
-} = require("./controllers/aiController");
-
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
 const { protect } = require("./middlewares/authMiddleware");
 
 const app = express();
 
-// ---------------- CORS Configuration ----------------
-const allowedOrigins = ["https://interview-prep-dxrh.onrender.com"];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-// Handle preflight (OPTIONS) requests
-app.options("*", cors());
-// -----------------------------------------------------
-
-// Connect to MongoDB
 connectDB();
 
-// JSON parser
 app.use(express.json());
 
-// Public Routes
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/questions", questionRoutes);
 
-// Protected AI Routes
+// // AI routes with auth protection middleware
 app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
 app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 
-// Start Server
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads"),{}));
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server Running on ${PORT}`);
 });
